289a31457780c307cc3c8b375a02a3829d5c4358
caleigh <caleighsteill@outlook.com>
2022-12-16 11:11:11 -0700

    commit tree 3fe3b1e1f17af2cd8d338bfe1b686f750b34a8bf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const cardsArray = []
+    for (let i = 0; i < names.length; i++) {
+        cardsArray.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+        debugger
+    }
+    return cardsArray
+}
+const names = ["Ali", "Ming", "Caleigh"]
+writeCards(names, "surprise")
+
+function countDown(numbers) {
+    let count = 10
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
+countDown(10)
